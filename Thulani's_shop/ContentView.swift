6a4471e9c2345c7452bfe7486e2a3a5893e79c8a
Thulani <theshikathulee@gmail.com>
2026-01-10 15:05:20 +0530

    Initial Commit

---
+++ Thulani's_shop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thulani's_shop
+//
+//  Created by COBSCCOMP242P-028 on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thulani's_shop/Thulani_s_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thulani_s_shopApp.swift
+//  Thulani's_shop
+//
+//  Created by COBSCCOMP242P-028 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Thulani_s_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
